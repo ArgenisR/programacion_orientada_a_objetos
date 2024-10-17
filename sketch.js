cc1a5de50d2a0a6df06fed7555696109b4b7a440
ArgenisR <argenis.ramos@ucuenca.edu.ec>
2024-10-17 14:18:39 -0500

    changes

--- sketch.js
+++ sketch.js
@@ -1,41 +1,23 @@
-let posX, posY;
-let velX, velY;
-let diametro;
-let rad;
-let backgroundC;
+let particulas = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
-  rad = ceil(random(20, 50));
-  diametro = rad * 2;
-  posX = random(rad, width - rad);
-  posY = random(rad, height - rad);
-
-  velX = (-5, 5);
-  velY = (-5, 5);
-  backgroundC = color(255, 210, 200);
 }
 
 function draw() {
-  background(backgroundC);
-  fill(255);
-  noStroke();
-  posX += velX;
-  posY += velY;
+  background(20, 10);
+  let nuevaParticula = new Particula(mouseX, mouseY);
+  particulas.push(nuevaParticula);
 
-  if (posX + rad > width || posX - rad <= 0) {
-    velX *= -1;
-    changeBgColor();
+  for (let i = 0; i < particulas.length; i++) {
+    particulas[i].update();
+    particulas[i].display();
   }
-
-  if (posY + rad > height || posY - rad <= 0) {
-    velY *= -1;
-    changeBgColor();
+  for (let i = 0; i < particulas.length; i++) {
+    if (particulas[i].estaViva) {
+    } else {
+      particulas.splice(i, 1);
+    }
   }
-
-  circle(posX, posY, diametro);
-}
-function changeBgColor() {
-  backgroundC = color(random(255), random(255), random(255));
+  console.log(particulas.length);
 }
