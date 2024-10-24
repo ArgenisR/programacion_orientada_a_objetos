738717a57df183c75f8c32a293b018618530b46c
ArgenisR <argenis.ramos@ucuenca.edu.ec>
2024-10-24 15:20:28 -0500

    updates

--- sketch.js
+++ sketch.js
@@ -22,4 +22,16 @@ function draw() {
   console.log(particulas.length);*/
 
   particulas = particulas.filter((pelota) => pelota.estaViva);
+
+  noFill();
+  stroke(252, 99, 145);
+
+  for (let i = 0; i < particulas.length - 1; i++) {
+    line(
+      particulas[i].posx,
+      particulas[i].posy,
+      particulas[i + 1].posx,
+      particulas[i + 1].posy
+    );
+  }
 }
